215a69914604cc9a801af6372bf059903c2b8734
Lisa Meyer <lisamermey@gmail.com>
2022-11-24 15:52:32 -0500

    Create Genre schema and export model

---
+++ models/genre.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const GenreSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 100
+  }
+});
+
+// virtual for Genre url
+GenreSchema.virtual('url').get(function () {
+  return `/catalog/genre/${this._id}`;
+});
+
+module.exports = mongoose.model('Genre', GenreSchema);
