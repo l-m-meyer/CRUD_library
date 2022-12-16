9bf26906b18fa03a174076a8b38c12986e33561e
Lisa Meyer <lisamermey@gmail.com>
2022-12-16 12:45:12 -0800

    Add virtual property to format date

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
 
 const Schema = mongoose.Schema;
 
@@ -21,6 +22,10 @@ const BookInstanceSchema = new Schema({
 // virtual for bookinstance's url
 BookInstanceSchema.virtual('url').get(function () {
   return `/catalog/bookinstance/${this._id}`;
+});
+
+BookInstanceSchema.virtual('due_back_formatted').get(function () {
+  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
 });
 
 module.exports = mongoose.model("BookInstance", BookInstanceSchema);
