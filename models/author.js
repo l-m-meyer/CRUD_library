4d711f4cba13919a3ded3a3ba84ecae6959faa38
Lisa Meyer <lisamermey@gmail.com>
2022-12-29 17:13:05 -0800

    Format date and time

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
 
 const Schema = mongoose.Schema;
 
@@ -32,6 +33,16 @@ AuthorSchema.virtual('name').get(function () {
 // Virtual for author's url
 AuthorSchema.virtual('url').get(function () {
   return `/catalog/author/${this._id}`;
+});
+
+// Virtual for formatting date of birth
+AuthorSchema.virtual('date_of_birth_formatted').get(function () {
+  return this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '';
+});
+
+// Virtual for formatting date of death
+AuthorSchema.virtual('date_of_death_formatted').get(function () {
+  return this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';
 });
 
 module.exports = mongoose.model('Author', AuthorSchema);
